a6d33138861ed6b7526aee43a043d88e6b0bcbb9
crank015 <renehaltaufderheide@yahoo.de>
2015-12-26 19:21:37 -0800

    Mute the Bot via Command

---
+++ mute.js
@@ -0,0 +1,29 @@
+ registerPlugin({
+    name: 'Mute',
+    version: '1.0',
+    description: 'Mutet den Bot',
+    author: 'Crank015',
+    vars: {
+
+	Volume: {
+            	title: 'Volume? (zwischen 1-100)',
+            	type: 'string'
+    }
+ }}, function(sinusbot, config) {
+	var v = config.Volume;
+
+		sinusbot.on('chat', function(ev){
+
+			switch(ev.msg) {
+				case '!mute' :
+					log('!mute')
+		                	setMute(true);
+				break;
+
+				case '!unmute' :
+					log('!unmute')
+		                	setMute(false)
+				break;
+				}
+		});	
+});
