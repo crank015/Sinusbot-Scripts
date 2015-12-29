db53e267dbf2e84d485842d6a6ef55a256615106
crank015 <renehaltaufderheide@yahoo.de>
2015-12-28 22:23:15 -0800

    If a User use a "! command" the Bot chat that to you

---
+++ AdminID.js
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2015 Crank015 <cranker015@gmail.com> & Lala Deviluke <latias@mail.latias.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * 
+ * @author Crank015 <cranker015@gmail.com>
+ * 
+ */
+
+registerPlugin({
+    name: 'sound',
+    version: '1.0.5',
+    description: 'Spielt Musik, oder schickt eine Naricht, wenn jemand in die Eingangshalle kommt',
+    author: 'Crank015 <cranker015@gmail.com>',
+    vars: {
+			AdminID:
+				title:'Admin UID',
+				Typ: 'string'
+	}
+	
+	}, function(sinusbot, config) {
+		
+		if (config.AdminID) {
+			
+			var adminid = config.AdminID.split(',');
+			
+		} else {
+			log('AdminID\'s not valid...');
+			var adminid = new Array();
+		}
+		
+	
+	
+	
+		sinusbot.on('chat', function(ev) {
+			
+			msg = ev.msg;
+			command = msg.indexOf('!')
+			
+				if (if msg == command);{
+					chatPrivate(AdminID, "User " + ev.clientNick + "hat einen Command genutzt");
+				}
+    });
+});
