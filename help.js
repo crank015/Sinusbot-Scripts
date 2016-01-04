a27d5c8b3c29197e02ce06abcc984aef1070de9d
crank015 <renehaltaufderheide@yahoo.de>
2016-01-04 16:08:32 +0100

    Script renamed

--- help.js
+++ help.js
@@ -1,5 +1,5 @@
 registerPlugin({
-    name: '!help Erweiterung',
+    name: '!hilfe',
     version: '0.1',
     description: 'Erweitert den Output des !help Commands',
     author: 'Crank015 <cranker015@gmail.com>',
