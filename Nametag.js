86b9af583bcb05cb3e9d53f0c3860ade12b5cf78
Crank015 <haltaufderheiderene@gmail.com>
2016-01-15 23:08:04 +0100

    Nickname Change

---
+++ Nametag.js
@@ -0,0 +1,21 @@
+registerPlugin({
+    name: 'Nametag',
+    version: '1.0',
+    description: 'Set\'s the Track title as nickname',
+    author: 'Crank015 <cranker015@gmail.com>',
+    vars: {}
+	
+}, function(sinusbot, config) {
+	
+	var t = track.title;
+	
+	sinusbot.on('track', function(ev) {
+		setNick(t);
+	});
+	
+	sinusbot.on('chat', function(ev) {
+		if (ev.msg == '.nickchange') {
+			´setNick('test')
+		}
+	});
+});
